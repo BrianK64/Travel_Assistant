b208d702a9acd99efdce12fb12a1c34f56609821
BrianK64 <kmsskyblue@gmail.com>
2024-06-06 21:07:39 +0900

    Comments Cleanup

--- main.py
+++ main.py
@@ -12,14 +12,11 @@ def main():
     # User's preferred language
     completed = 0
     while not completed:
-        print("\n###top of the while loop###\n")
         lang_id = input("Select your language: ")
         if lang_id in lang:
-            print("\n###inside if statement###\n")
             lang_id = lang[lang_id]
             completed = 1
         else:
-            print("\n###inside else statement###\n")
             print("Selected language not supported\n")
 
     # Get the destination from user
