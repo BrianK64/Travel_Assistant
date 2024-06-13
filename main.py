9a280f195c309a049e1aba1b2389d1580cf92d21
BrianK64 <kmsskyblue@gmail.com>
2024-06-13 21:07:40 +0900

    Replace bs4 package with Selenium

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # IMPORTS
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 
 # Mapping languages to their corresponding codes
 lang = {
@@ -40,13 +43,37 @@ def main():
     url = f"https://www.booking.com/searchresults.html?ss={city_id}&lang={lang_id}&checkin={checkin}&checkout={checkout}&group_adults={group_adults}&no_rooms={no_rooms}&group_children={group_children}"
     print("\nURL: "+url+"\n")
 
-    # Send a GET request to the URL
-    response = requests.get(url)
+    # Setup webdriver
+    # Create a new Selenium Service object which represents the WebDriver's connection to the browser's instance
+    # This will automatically manage the ChromeDriver executable, which is needed by Selenium
+    s = Service(ChromeDriverManager().install())
 
-    # Parse the HTML content of the URL
-        ## response.text is the raw HTML content of the web page fetched via requests
-        ## 'html.parser' argument tells BeautifulSoup to use HTML parser to parse the document.
-    soup = BeautifulSoup(response.text, "html.parser")
+    # Create a Chrome WebDriver instance
+    # Webdriver is a component of Selenium that interacts with the web browser
+    driver = webdriver.Chrome(service=s)
+
+    # Request the WebDriver to navigate to the URL.
+    driver.get(url)
+
+    # Wait for the dynamic content to load
+    time.sleep(5)
+
+    # Scroll down to the bottom of the page
+    ## The JavaScript code "window.scrollTo(0, document.body.scrollHeight)" scrolls to the bottom of the
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait for the new content to load
+    time.sleep(5)
+
+    # Access page source (HTML)
+    html = driver.page_source
+
+    # Write the content of the HTML to a seprate text file.
+    with open("assets/booking.html", 'w', encoding='utf-8') as file:
+        file.write(html)
+
+    # Close the browser
+    driver.quit()
 
     return True
 
